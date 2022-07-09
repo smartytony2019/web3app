30298114d0fb3bd6861738b78e5b205e6a820391
tony <tony@gmail.com>
2022-07-09 19:30:19 +0400

    fixed

--- cb_v1/src/main/java/com/xinbo/chainblock/annotation/RequiredPermission.java
+++ cb_v1/src/main/java/com/xinbo/chainblock/annotation/RequiredPermission.java
@@ -1,10 +1,13 @@
 package com.xinbo.chainblock.annotation;
 
-
 import com.xinbo.chainblock.enums.PermissionCodeEnum;
-
 import java.lang.annotation.*;
 
+/**
+ * @author tony
+ * @date 2022/6/28 20:00
+ * @desc 权限验证
+ */
 @Target({ElementType.TYPE,ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
