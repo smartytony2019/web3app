cb026787541d35c1d699d44572c37800312d2d92
tony <tony@gmail.com>
2022-07-11 20:17:23 +0400

    fixed

--- cb_admin/src/lang/en.js
+++
@@ -1,6 +0,0 @@
-export default {
-  message: {
-    'hello': 'hello, world',
-    'msg': 'point out'
-  }
-}
--- cb_admin/src/lang/zh.js
+++
@@ -1,8 +0,0 @@
-const zh = {
-  message: {
-    'hello': '你好，世界',
-    'msg': '提示'
-  }
-}
-
-export default zh
