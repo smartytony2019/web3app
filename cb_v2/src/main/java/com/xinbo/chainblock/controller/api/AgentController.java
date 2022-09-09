5c9e0a18e7d7cc3e4d26cb276cf89718d9021e33
tony <tony@gmail.com>
2022-09-09 20:02:27 +0400

    fixed

--- cb_v2/src/main/java/com/xinbo/chainblock/controller/api/AgentController.java
+++ cb_v2/src/main/java/com/xinbo/chainblock/controller/api/AgentController.java
@@ -48,6 +48,34 @@ public class AgentController {
     @Autowired
     private StatisticsService statisticsService;
 
+    @Operation(summary = "find", description = "代理佣金")
+    @PostMapping("find/{date}")
+    public R<Object> find(@PathVariable String date) {
+
+        JwtUserBo jwtUser = JwtUtil.getJwtUser();
+        AgentEntity agentEntity = agentService.findByUid(jwtUser.getUid());
+        if (ObjectUtils.isEmpty(agentEntity) || agentEntity.getId() <= 0) {
+            return R.builder().data(StatusCode.FAILURE).build();
+        }
+
+        AgentCommissionEntity entity = agentCommissionService.find(jwtUser.getUid(), date);
+
+        float commissionTotal = agentCommissionService.findCommissionTotal(jwtUser.getUid());
+        float availableCommission = agentCommissionService.findAvailableCommission(jwtUser.getUid());
+
+
+        JSONObject result = new JSONObject();
+
+        result.put("directCount", entity.getDirectCount());
+        result.put("teamCount", entity.getTeamCount());
+        result.put("teamPerformance", entity.getTeamPerformance());
+        result.put("totalPerformance", entity.getTotalPerformance());
+
+        result.put("commissionTotal",commissionTotal);
+        result.put("availableCommission",availableCommission);
+
+        return R.builder().code(StatusCode.SUCCESS).data(result).build();
+    }
 
     @JwtIgnore
     @Operation(summary = "myTeam", description = "我的团队")
@@ -55,12 +83,12 @@ public class AgentController {
     public R<Object> myTeam() {
         JSONObject result = new JSONObject();
 
-        int uid = 2;
+        JwtUserBo jwtUser = JwtUtil.getJwtUser();
 
-        AgentEntity agentEntity = agentService.findByUid(uid);
+        AgentEntity agentEntity = agentService.findByUid(jwtUser.getUid());
 
         //直属人数
-        List<AgentEntity> direct = agentService.direct(uid);
+        List<AgentEntity> direct = agentService.direct(jwtUser.getUid());
         result.put("directNum", direct.size());
 
         //团队人数
@@ -98,7 +126,7 @@ public class AgentController {
 
         String yesterday = DateUtil.yesterday().toString(GlobalConst.DATE_YMD);
         AgentCommissionEntity entity = AgentCommissionEntity.builder()
-                .uid(uid)
+                .uid(jwtUser.getUid())
                 .date(yesterday)
                 .build();
         AgentCommissionEntity commissionEntity = agentCommissionService.find(entity);
@@ -117,13 +145,13 @@ public class AgentController {
         result.put("yesterdayCommissionTotal", totalCommission);
 
 
-        float availableCommission = agentCommissionService.findAvailableCommission(uid);
+        float availableCommission = agentCommissionService.findAvailableCommission(jwtUser.getUid());
 
         //可用佣金
         result.put("availableCommission", availableCommission);
 
         //佣金合计
-        float commissionTotal = agentCommissionService.findCommissionTotal(uid);
+        float commissionTotal = agentCommissionService.findCommissionTotal(jwtUser.getUid());
         result.put("commissionTotal", commissionTotal);
 
         return R.builder().code(StatusCode.SUCCESS).data(result).build();
@@ -136,11 +164,11 @@ public class AgentController {
     public R<Object> myPerformance() {
         JSONObject result = new JSONObject();
 
-        int uid = 2;
-        AgentEntity agentEntity = agentService.findByUid(uid);
+        JwtUserBo jwtUser = JwtUtil.getJwtUser();
+        AgentEntity agentEntity = agentService.findByUid(jwtUser.getUid());
 
         // 直属人数
-        List<AgentEntity> direct = agentService.direct(uid);
+        List<AgentEntity> direct = agentService.direct(jwtUser.getUid());
         result.put("directNum", direct.size());
 
         // 团队人数
@@ -154,7 +182,7 @@ public class AgentController {
         // 今日总业绩
         AgentCommissionEntity commissionEntity = AgentCommissionEntity.builder()
                 .date(DateUtil.date().toString(GlobalConst.DATE_YMD))
-                .uid(uid)
+                .uid(jwtUser.getUid())
                 .build();
         AgentCommissionEntity agentCommissionEntity = agentCommissionService.find(commissionEntity);
         result.put("todayPerformanceTotal", agentCommissionEntity.getTotalPerformance());
@@ -169,15 +197,15 @@ public class AgentController {
     @PostMapping("teamPerformance/{date}")
     public R<Object> teamPerformance(@PathVariable String date) {
         JSONObject result = new JSONObject();
-
-        int uid = 2;
-        AgentCommissionEntity entity = agentCommissionService.find(uid, date);
-        result.put("teamPerformance", entity.getTeamPerformance());
-        result.put("selfPerformance", entity.getSelfPerformance());
-        result.put("directPerformance", entity.getDirectPerformance());
-        result.put("subPerformance", entity.getSubPerformance());
-        result.put("totalCommission", entity.getTotalCommission());
-
+        JwtUserBo jwtUser = JwtUtil.getJwtUser();
+        AgentCommissionEntity entity = agentCommissionService.find(jwtUser.getUid(), date);
+        if(!ObjectUtils.isEmpty(entity)) {
+            result.put("teamPerformance", entity.getTeamPerformance());
+            result.put("selfPerformance", entity.getSelfPerformance());
+            result.put("directPerformance", entity.getDirectPerformance());
+            result.put("subPerformance", entity.getSubPerformance());
+            result.put("totalCommission", entity.getTotalCommission());
+        }
         return R.builder().code(StatusCode.SUCCESS).data(result).build();
     }
 
@@ -212,28 +240,6 @@ public class AgentController {
             }
 
             String sn = IdUtil.getSnowflake().nextIdStr();
-
-//            // Step 2.1: 会员表
-//            MemberEntity member = MemberEntity.builder()
-//                    .money(totalCommission)
-//                    .id(memberEntity.getId())
-//                    .version(memberEntity.getVersion())
-//                    .build();
-//
-//
-//            // Step 2.2: 会员流水表
-//            MemberFlowEntity memberFlow = MemberFlowEntity.builder()
-//                    .sn(sn)
-//                    .uid(memberEntity.getId())
-//                    .username(memberEntity.getUsername())
-//                    .beforeMoney(memberEntity.getMoney())
-//                    .afterMoney(memberEntity.getMoney() + totalCommission)
-//                    .flowMoney(totalCommission)
-//                    .item(MemberFlowItemEnum.AGENT_COMMISSION.getName())
-//                    .itemCode(MemberFlowItemEnum.AGENT_COMMISSION.getCode())
-//                    .itemZh(MemberFlowItemEnum.AGENT_COMMISSION.getNameZh())
-//                    .createTime(DateUtil.date())
-//                    .build();
 
             // Step 2.2: 代理佣金记录表
             AgentCommissionRecordEntity entity = AgentCommissionRecordEntity.builder()
