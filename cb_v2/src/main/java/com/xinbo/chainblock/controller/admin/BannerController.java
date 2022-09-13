00f9c9ecc5d02c639e0a71064ceaf7cf5c28fe23
wufeng <lht774029024@gmail.com>
2022-09-13 00:55:51 -0400

    12

--- cb_v2/src/main/java/com/xinbo/chainblock/controller/admin/BannerController.java
+++ cb_v2/src/main/java/com/xinbo/chainblock/controller/admin/BannerController.java
@@ -4,9 +4,7 @@ import cn.hutool.core.date.DateUtil;
 import com.xinbo.chainblock.consts.StatusCode;
 import com.xinbo.chainblock.dto.BannerDto;
 import com.xinbo.chainblock.entity.admin.BannerEntity;
-import com.xinbo.chainblock.entity.admin.LanguageEntity;
 import com.xinbo.chainblock.service.BannerService;
-import com.xinbo.chainblock.service.LangService;
 import com.xinbo.chainblock.utils.MapperUtil;
 import com.xinbo.chainblock.utils.R;
 import com.xinbo.chainblock.vo.BannerVo;
@@ -25,8 +23,6 @@ import java.util.stream.Collectors;
 public class BannerController {
     @Autowired
     private BannerService bannerService;
-    @Autowired
-    private LangService langService;
 
     @Operation(summary = "insert", description = "新增轮播图")
     @PostMapping("insert")
@@ -37,7 +33,7 @@ public class BannerController {
         return R.builder().code(isSuccess ? StatusCode.SUCCESS : StatusCode.FAILURE).build();
     }
 
-    @Operation(summary = "updateOrDeleteBanner", description = "更新或删除轮播图")
+    @Operation(summary = "updateOrDelete", description = "更新或删除轮播图")
     @PostMapping("updateOrDelete")
     public R<Object> updateOrDelete(@RequestBody BannerVo vo){
         BannerEntity entity = MapperUtil.to(vo, BannerEntity.class);
@@ -45,10 +41,10 @@ public class BannerController {
         return R.builder().code(isSuccess ? StatusCode.SUCCESS : StatusCode.FAILURE).build();
     }
 
-    @Operation(summary = "findAllBanner", description = "获取所有轮播图")
+    @Operation(summary = "findAll", description = "获取所有轮播图")
     @PostMapping("findAll")
     public R<Object> findAll(){
-        List<BannerEntity> bannerEntityList = bannerService.findAll().stream().filter(  item -> {
+        List<BannerEntity> bannerEntityList = bannerService.findAll().stream().filter(item -> {
             if(item != null) return item.getIsEnable();
             return true;
         }).collect(Collectors.toList());
