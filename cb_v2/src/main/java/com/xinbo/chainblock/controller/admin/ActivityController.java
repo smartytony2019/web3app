966eb6f81b259bcf0f98e8f08c89bf48d653c0d7
wufeng <lht774029024@gmail.com>
2022-10-03 07:19:38 -0400

    11

--- cb_v2/src/main/java/com/xinbo/chainblock/controller/admin/ActivityController.java
+++ cb_v2/src/main/java/com/xinbo/chainblock/controller/admin/ActivityController.java
@@ -6,32 +6,18 @@ import cn.hutool.core.util.IdUtil;
 import com.xinbo.chainblock.annotation.RequiredPermission;
 import com.xinbo.chainblock.bo.BasePageBo;
 import com.xinbo.chainblock.bo.EnumItemBo;
-import com.xinbo.chainblock.bo.JwtUserBo;
 import com.xinbo.chainblock.consts.StatusCode;
 import com.xinbo.chainblock.dto.ActivityDto;
-import com.xinbo.chainblock.dto.PermissionDto;
-import com.xinbo.chainblock.dto.UserDto;
 import com.xinbo.chainblock.entity.activity.ActivityEntity;
-import com.xinbo.chainblock.entity.activity.ActivityRuleEntity;
-import com.xinbo.chainblock.entity.activity.ActivityRuleItemEntity;
-import com.xinbo.chainblock.entity.admin.PermissionEntity;
-import com.xinbo.chainblock.entity.admin.UserEntity;
 import com.xinbo.chainblock.enums.*;
 import com.xinbo.chainblock.service.ActivityService;
-import com.xinbo.chainblock.service.UserService;
-import com.xinbo.chainblock.utils.JwtUtil;
 import com.xinbo.chainblock.utils.MapperUtil;
 import com.xinbo.chainblock.utils.R;
-import com.xinbo.chainblock.vo.ActivityRuleItemVo;
-import com.xinbo.chainblock.vo.ActivityRuleVo;
 import com.xinbo.chainblock.vo.ActivityVo;
-import com.xinbo.chainblock.vo.UserVo;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.*;
 
 @RestController("adminActivityController")
 @RequestMapping("/admin/activity")
